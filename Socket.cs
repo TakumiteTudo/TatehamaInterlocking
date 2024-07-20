695ea55ac8702e716fb885034e20add5a9b46868
kesigomon@gmail.com <34025522+kesigomon@users.noreply.github.com>
2024-07-20 14:16:18 +0900

    socket.cs忘れてた

---
+++ Socket.cs
@@ -0,0 +1,52 @@
+﻿namespace TatehamaInterlocking;
+using SocketIOClient;
+
+public class TrackCircuitInfo
+{
+   public string name { get; set; }
+   public double startMeter { get; set; }
+   public double endMeter { get; set; }
+   public string signalType { get; set; }
+}
+
+public class Socket
+{
+
+   private SocketIO client;
+   public Socket(string serverAddress)
+   {
+      client = new SocketIO(serverAddress);
+      Task.Run(async () => await client.ConnectAsync());
+   }
+
+
+   public async Task connect()
+   {
+      await client.ConnectAsync();
+   }
+
+   public async Task<string?> routeOpen(string signalName)
+   {
+      await client.EmitAsync("routeOpen", signalName);
+      var tcs = new TaskCompletionSource<string?>();
+      client.On("routeOpenResult", response =>
+      {
+         var elapsedData = response.GetValue<string?>();
+         tcs.SetResult(elapsedData);
+      });
+      return await tcs.Task;
+   }
+   
+   public async Task<List<TrackCircuitInfo>> getAllSignal()
+   {
+      await client.EmitAsync("getAllSignal");
+      var ecs = new TaskCompletionSource<List<TrackCircuitInfo>>();
+      client.On("getAllSignalResult", response =>
+      {
+         var route = response.GetValue<List<TrackCircuitInfo>>();
+         ecs.SetResult(route);
+      });
+      return await ecs.Task; 
+   }
+   
+}
